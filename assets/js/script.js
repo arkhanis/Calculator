19c66078fb620611de9dee5e9041bd36f62a31b9
arkhanis <casanovac@gmail.com>
2023-12-01 14:59:49 -0300

    finalizando las funciones en js

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,14 @@ buttons.forEach((item) => {
       display.innerText = "";
     } else if (item.id == "backspace") {
       let string = display.innerText.toString();
-      display.innerText = string.substr(0,string.length-1)
-      
-    } else if (display.innerText !="" && item.id=="eqyual"){
-        display.innerText = eval()
+      display.innerText = string.substr(0, string.length - 1);
+    } else if (display.innerText != "" && item.id == "equal") {
+      display.innerText = eval(display.innerText);
+    } else if (display.innerText == "" && item.id == "equal") {
+      display.innerText = "Null";
+      setTimeout(() => (display.innerText = ""), 2000);
+    } else {
+      display.innerText += item.id;
     }
   };
 });
